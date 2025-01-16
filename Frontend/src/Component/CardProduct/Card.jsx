6379f42f03d36985712cdceb63ad5a4bef5311bb
Thiraphat2006 <thiraphaearth@gmail.com>
2025-01-17 00:11:31 +0700

    delete Backend
    ทำไม่ทันจร้า เพิ่มการ์ดด้วยเจ๋งง

--- Frontend/src/Component/CardProduct/Card.jsx
+++ Frontend/src/Component/CardProduct/Card.jsx
@@ -1,17 +1,18 @@
 import './card.css'
+import DataProduct from '../../../public/Data/ข้อมูลครืมตุ่งๆ.json'
 // card ที่ใช้ในหน้า product
-const Card = ({ img, title, Type, ID }) => {
+const Card = ({ Image, ProductNameEng, Type, }) => {
     return (
         <>
             <div className="card ">
-                <div className="card-con grid grid-cols-2 gap-4">
-                    <div className="img bg-gray-950 h-auto">
-                        <img src={img} alt="ImageError" />
+                <div className="card-con m-5 ">
+                    <div className="img">
+                        <img src={Image} alt="ImageError" />
                     </div>
-                    <div className="text">
-                        <span>{title}</span>
-                        <div className="type flex">
-                            <span>{Type}</span>
+                    <div className="text max-w-80 ml-5">
+                        <p className='nameproduct'>{ProductNameEng}</p>
+                        <div className="type inline-block px-2 py-4 rounded-2xl">
+                            <p className='typeproduct'>{Type}</p>
                         </div>
                     </div>
                 </div>
@@ -19,19 +20,33 @@ const Card = ({ img, title, Type, ID }) => {
         </>
     )
 }
-//ข้อมูลปลอมๆ ที่ใช้ในการทดสอบ
-const test = [
-    { id: 1, title: "Garnier Bright Complete Vitamin C Face Wash Scrub", Type: "WHAT" },
-    { id: 2, title: "Garnier Turbo Bright Super Duo Foam" }
-]
-//เก็บรวม card ทั้งหมด (เดี๋ยวต่อฐานข้อมูลมา)
+
+//card ที่ map ข้อมูลที่เราเลือกมา
+export const Mapcard = ({filter}) => {
+    return (
+        <>
+        {
+            DataProduct
+                .filter(data => data.Type === `${filter}`)
+                .map((data) => (
+                    <Card {...data} key={data.KEY} />
+                ))
+        }
+        </>
+    )
+}
+
+
+//เก็บรวม card ทั้งหมด
 const Cardcontainer = () => {
     return (
         <div className="allcard mt-10">
             <div className="cardcontainer grid  gap-10 grid-cols-1 justify-center w-4/5 md:grid-cols-2">
-                {test.map((data) => (
-                    <Card {...data} key={data.id} />
-                ))}
+                <Mapcard filter='ครีนเซอร์' />
+                <Mapcard filter='คลีนซิ่ง' />
+                <Mapcard filter='เซรั่ม' />
+                <Mapcard filter='โทนเนอร์' />
+                <Mapcard filter='โลชั่น' />
             </div>
         </div>
 
--- Frontend/src/Component/about/Aboutcomponent.jsx
+++ Frontend/src/Component/about/Aboutcomponent.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import './Aboutcomponent.css'
-import TEST from "../../Image/BG/man01.jpg"
+import TEST from "../../../public/Image/BG/man01.jpg"
 function Aboutcomponent() {
+    const DataProfile = [
+        {
+            IDcard: 'card1',
+            name: 'สุปรียา ทาทน',
+            no: 36,
+            // purpose: 'จัดทำ แก้ไข รูปเล่ม'
+        },
+        {
+            IDcard: 'card2',
+            name: 'ธีรภัทร บูรณ์พิภพธนโชติ',
+            no: 34,
+            // purpose: 'จัดทำ แก้ไข รูปเล่ม'
+        },
+        {
+            IDcard: 'card3',
+            name: 'ฟ้าใส ใจเกินร้อย',
+            no: 29,
+            // purpose: 'จัดทำ แก้ไข รูปเล่ม'
+        }
+    ]
+
     return (
         <>
             <Toptext />
             <div className="allcardprofile w-4/5 flex justify-between mt-7 ml-40">
-                <CardProfile
-                    name={`สุปรียา ทาทน`} no={36} 
-                    purpose={`จัดทำ แก้ไข รูปเล่ม`}/>
-                <CardProfile name={`ธีรภัทร บูรณ์พิภพธนโชติ`} no={34} />
-                <CardProfile name={`ฟ้าใส ใจเกินร้อย`} no={29} />
+                {DataProfile.map((item, index) => {
+                    return (
+                        <CardProfile key={index} IDcard={item.IDcard} name={item.name} no={item.no} purpose={item.purpose} />
+                    )
+                })}
             </div>
         </>
     )
@@ -19,16 +40,16 @@ function Aboutcomponent() {
 export default Aboutcomponent
 
 //CardProfile
-export const CardProfile = ({ name, no, purpose }) => {
+export const CardProfile = ({ IDcard , name, no, purpose }) => {
     return (
         <>
-            <div className="cardprofile ">
+            <div className="cardprofile " id={IDcard}>
                 <div className="card-con">
                     <img src={TEST} alt="" />
                     <div className="text-con">
                         <h1>{name}</h1>
                         <p>ปวช. 3/1 คอมฯ เลขที่ {no}</p>
-                        <h2>หน้าที่</h2>
+                        {/* <h2>หน้าที่</h2> */}
                         <p>{purpose}</p>
                     </div>
                 </div>
